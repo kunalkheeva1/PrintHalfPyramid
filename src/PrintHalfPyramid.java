762d143586ad78ab9a6100e016cd96798333f1df
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-02 22:40:43 -0700

    added comments for better understandings

--- src/PrintHalfPyramid.java
+++ src/PrintHalfPyramid.java
@@ -1,11 +1,14 @@
 public class PrintHalfPyramid {
 
     public static void printHalfPyramid(int row){
-
+        //until reaches row number
         for(int i=1; i<=row; i++){
+            // if i = 1, j=1 and will print *, but wont print more
+            // as j's limit is till i, so for every iteration its i numbers of * only.
             for(int j= 1; j<=i; j++){
                 System.out.print("* ");
-            }System.out.println();
+            }//cursor changers everytime it prints one iteration
+            System.out.println();
         }
         System.out.println();
 
