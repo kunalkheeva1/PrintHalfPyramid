ec3246eb02befb6e3faa5225fae38fa35b5d6031
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-02 22:38:12 -0700

    created a method to print the half pyramid, also to print it on the right side we just need to change the inner loop limits and start it from the i

--- src/PrintHalfPyramid.java
+++ src/PrintHalfPyramid.java
@@ -1,5 +1,19 @@
 public class PrintHalfPyramid {
+
+    public static void printHalfPyramid(int row){
+
+        for(int i=1; i<=row; i++){
+            for(int j= 1; j<=i; j++){
+                System.out.print("* ");
+            }System.out.println();
+        }
+        System.out.println();
+
+    }
+
+
     public static void main(String[] args) {
-        
+        printHalfPyramid(4);
+
     }
 }
